e76c5283ccd6e8439cf6f2bf95e1050a0a3ef5da
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-15 15:56:10 -0300

    Requisito 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+
 function App() {
   return (
-    <p>Trybetunes</p>
+    <>
+      <p>Trybetunes</p>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Login.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { createUser } from '../services/userAPI';
+
+function Login() {
+  const [buttonHabil, setButtonHabil] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const verify = target.value.length >= 3 && setButtonHabil(true);
+    setInputValue(target.value);
+    return verify;
+  };
+  const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    await createUser({ name: inputValue });
+    setLoading(false);
+  };
+  if (loading) {
+    return <h2>Carregando...</h2>;
+  }
+
+  return (
+    <form>
+      <label htmlFor="name">Nome</label>
+      <input
+        type="text"
+        id="name"
+        data-testid="login-name-input"
+        onChange={ handleChange }
+      />
+      { buttonHabil
+        ? <button
+            data-testid="login-submit-button"
+            onClick={ handleClick }
+        >
+          Entrar
+          </button>
+        : <button
+            data-testid="login-submit-button"
+            disabled
+            onClick={ handleClick }
+        >
+          Entrar
+          </button>}
+    </form>
+  );
+}
+
+export default Login;
