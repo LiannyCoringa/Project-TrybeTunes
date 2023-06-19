94b9a1764dbf0578fe9ae488fafb275c6811de6b
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-19 13:51:05 -0300

    Iniciando o requisito 10

--- src/components/ProfileEdit.tsx
+++ src/components/ProfileEdit.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../services/userAPI";
+
 function ProfileEdit() {
+  const [loading, setLoading] = useState(true);
+  const [infosUser, setInfosUser] = useState({
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+  });
+  useEffect(() => {
+    const user = async () => {
+      const infos = await getUser();
+      setLoading(false);
+      setInfosUser(infos);
+    };
+    user();
+  }, []);
+  if (loading) {
+    return <h2>Carregando...</h2>;
+  }
   return (
     <h1>Editar</h1>
   );
